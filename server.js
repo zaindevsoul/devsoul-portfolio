14c16fe766b990611ff62b95a6cbd2d4947eb7bd
Zain Ul Islam Zia <155431761+zaindevsoul@users.noreply.github.com>
2025-03-19 12:44:14 +0500

    updated server.js

--- server.js
+++ server.js
@@ -97,11 +97,3 @@ app.post("/api/contact", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-process.on("SIGTERM", () => {
-  console.log("SIGTERM received. Shutting down gracefully...");
-  server.close(() => {
-    console.log("Server closed.");
-    process.exit(0);
-  });
-});
