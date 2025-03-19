d141e04b0cf23ef784d6ef086c1bc056925664eb
Zain Ul Islam Zia <155431761+zaindevsoul@users.noreply.github.com>
2025-03-19 13:03:34 +0500

    updated cors

--- server.js
+++ server.js
@@ -9,7 +9,13 @@ const Joi = require("joi");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://portfolio.devsoul.net/"],
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 app.use(bodyParser.json());
 
 const contactSchema = Joi.object({
